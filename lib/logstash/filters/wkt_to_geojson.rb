adb28bda525c3e628613042a654ef33d8e391f77
kalmas <kyle.almas@dominionenterprises.com>
2017-06-30 17:18:06 -0400

    add error handling

--- lib/logstash/filters/wkt_to_geojson.rb
+++ lib/logstash/filters/wkt_to_geojson.rb
@@ -3,30 +3,40 @@ require "logstash/filters/base"
 require "logstash/namespace"
 require "rgeo"
 require "rgeo-geojson"
+require "json"
 
 class LogStash::Filters::WktToGeojson < LogStash::Filters::Base
 
   config_name "wkt_to_geojson"
 
-  config :in_field, :validate => :string, :default => "message"
-  config :out_field, :validate => :string
+  config :field, :validate => :string, :required => true
+  config :target, :validate => :string
+  config :tag_on_parse_failure, :validate => :array, :default => [ '_wkt_parse_failure' ]
 
   public
   def register
     @parser = RGeo::WKRep::WKTParser.new()
+    @encoder = RGeo::GeoJSON.coder()
   end # def register
 
   public
   def filter(event)
 
-    if @out_field.nil?
-      @out_field = @in_field
+    if @target.nil?
+      @target = @field
     end
 
-    geo = @parser.parse(event.get(@in_field))
-    json = RGeo::GeoJSON.encode(geo)
+    wkt = event.get(@field)
 
-    event.set(@out_field, json.to_s)
+    begin
+      geo = @parser.parse(wkt)
+      json = RGeo::GeoJSON.encode(geo)
+      event.set(@target, json.to_json)
+    rescue Exception => e
+      @logger.error('WKT Parse Error',
+          :wkt => wkt, :exception => e)
+      @tag_on_parse_failure.each { |tag| event.tag(tag) }
+    end
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
--- spec/filters/wkt_to_geojson_spec.rb
+++ spec/filters/wkt_to_geojson_spec.rb
@@ -6,47 +6,50 @@ describe LogStash::Filters::WktToGeojson do
   describe "Convert WKT to GeoJSON" do
     let(:config) do <<-CONFIG
       filter {
-        wkt_to_geojson {}
+        wkt_to_geojson {
+            field => "geometry"
+        }
       }
     CONFIG
     end
 
-    sample("message" => "POLYGON ((0.0 0.0, 5.0 0.0, 5.0 5.0, 0.0 5.0, 0.0 0.0), (2.0 2.0, 2.0 3.0, 3.0 3.0, 3.0 2.0, 2.0 2.0))") do
-      expect(subject).to include("message")
-      expect(subject.get('message')).to eq("{\"type\"=>\"Polygon\", \"coordinates\"=>[[[0.0, 0.0], [5.0, 0.0], [5.0, 5.0], [0.0, 5.0], [0.0, 0.0]], [[2.0, 2.0], [2.0, 3.0], [3.0, 3.0], [3.0, 2.0], [2.0, 2.0]]]}")
+    sample("geometry" => "POLYGON ((0.0 0.0, 5.0 0.0, 5.0 5.0, 0.0 5.0, 0.0 0.0), (2.0 2.0, 2.0 3.0, 3.0 3.0, 3.0 2.0, 2.0 2.0))") do
+      expect(subject.get('geometry')).to eq("{\"type\":\"Polygon\",\"coordinates\":[[[0.0,0.0],[5.0,0.0],[5.0,5.0],[0.0,5.0],[0.0,0.0]],[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]}")
     end
   end
 
-  describe "Read from input field" do
+  describe "Write to target field" do
     let(:config) do <<-CONFIG
       filter {
         wkt_to_geojson {
-          in_field => "wkt"
+          field => "geometry"
+          target => "geo_json"
         }
       }
     CONFIG
     end
 
-    sample("wkt" => "Point(-122.1 47.2)") do
-      expect(subject).to include("wkt")
-      expect(subject.get('wkt')).to eq("{\"type\"=>\"Point\", \"coordinates\"=>[-122.1, 47.2]}")
+    sample("geometry" => "GeometryCollection(Point(3 5), LineString(-2 0, -3 -4))") do
+      expect(subject.get('geo_json')).to eq("{\"type\":\"GeometryCollection\",\"geometries\":[{\"type\":\"Point\",\"coordinates\":[3.0,5.0]},{\"type\":\"LineString\",\"coordinates\":[[-2.0,0.0],[-3.0,-4.0]]}]}")
+      expect(subject.get('geometry')).to eq("GeometryCollection(Point(3 5), LineString(-2 0, -3 -4))")
     end
   end
 
-  describe "Write to output field" do
-      let(:config) do <<-CONFIG
-        filter {
-          wkt_to_geojson {
-            out_field => "geo_json"
-          }
+  describe "Handle invalid input" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          field => "geometry"
+          target => "geo_json"
         }
-      CONFIG
-      end
-
-      sample("message" => "GeometryCollection(Point(3 5), LineString(-2 0, -3 -4))") do
-        expect(subject).to include("geo_json")
-        expect(subject.get('geo_json')).to eq("{\"type\"=>\"GeometryCollection\", \"geometries\"=>[{\"type\"=>\"Point\", \"coordinates\"=>[3.0, 5.0]}, {\"type\"=>\"LineString\", \"coordinates\"=>[[-2.0, 0.0], [-3.0, -4.0]]}]}")
-        expect(subject.get('message')).to eq("GeometryCollection(Point(3 5), LineString(-2 0, -3 -4))")
-      end
+      }
+    CONFIG
     end
+
+    sample("geometry" => "MULTIPOLYGON (((40, 20, 45, 40)))") do
+      expect(subject).to_not include("geo_json")
+      expect(subject.get('geometry')).to eq("MULTIPOLYGON (((40, 20, 45, 40)))")
+      expect(subject.get('tags')).to include('_wkt_parse_failure')
+    end
+  end
 end
